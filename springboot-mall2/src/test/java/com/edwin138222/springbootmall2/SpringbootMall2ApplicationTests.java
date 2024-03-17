8c94f86dfebc64ecf436aabb99649621acc7c8a6
linqunren <edwin138222@yahoo.com.tw>
2024-03-17 12:46:25 +0800

    初始化spring boot project

---
+++ springboot-mall2/src/test/java/com/edwin138222/springbootmall2/SpringbootMall2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin138222.springbootmall2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMall2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
